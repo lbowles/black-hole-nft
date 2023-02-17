25ecdb6fe9a15219baf7bf8ca08f1391e205fde3
lbowles <lspbowles@gmail.com>
2023-02-17 18:17:39 +0200

    added rainbow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,60 @@
-import logo from "./logo.svg"
 import "./App.css"
 import { Navbar } from "./components/Navbar/Navbar"
-import { useState } from "react"
+import "@rainbow-me/rainbowkit/styles.css"
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi"
+import { publicProvider } from "wagmi/providers/public"
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home/Home"
+import * as allChains from "@wagmi/chains"
+import deployments from "./deployments.json"
 
-type pagesType = { name: string; link: string; active: boolean }[]
-const defaultPages = [
-  { name: "HOME", link: "/", active: true },
-  { name: "MINT", link: "/mint", active: false },
-  { name: "BURN", link: "/burn", active: false },
-]
+// Loop over all chains and check if they match deployments.chainId
+const deployedChain = Object.values(allChains).filter((chain) => {
+  return deployments.chainId === chain.id.toString()
+})[0]
+
+console.log(deployedChain)
+
+// Check if in development
+const isDev = process.env.NODE_ENV === "development"
+
+const { chains, provider, webSocketProvider } = configureChains([deployedChain], [publicProvider()])
+
+const { connectors } = getDefaultWallets({
+  appName: "BlackHoles",
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+})
 
 function App() {
-  const [pages, setPages] = useState<pagesType>(defaultPages)
-
   return (
     <div className="flex justify-center w-screen">
-      <Navbar pages={pages} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          showRecentTransactions={true}
+          chains={chains}
+          theme={{
+            ...darkTheme({ accentColor: "#393A3E" }),
+            fonts: {
+              body: "VT323, monospace",
+            },
+          }}
+        >
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mint" element={<div className="text-white">mint</div>} />
+            <Route path="/burn" element={<div>burn</div>} />
+          </Routes>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </div>
   )
 }
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,33 @@
-type INavbar = {
-  pages: { name: string; link: string; active: boolean }[]
-}
-export const Navbar = ({ pages }: INavbar) => {
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+
+type pagesType = { name: string; link: string; active: boolean }[]
+
+const defaultPages = [
+  { name: "HOME", link: "/", active: true },
+  { name: "MINT", link: "/mint", active: false },
+  { name: "BURN", link: "/burn", active: false },
+]
+
+export const Navbar = () => {
+  const navigate = useNavigate()
+  const [pages, setPages] = useState<pagesType>(defaultPages)
+
+  const setActive = (i: number) => {
+    const updatedPages = pages.map((p, index) => {
+      if (index === i) {
+        return { ...p, active: true }
+      } else {
+        return { ...p, active: false }
+      }
+      return p
+    })
+    setPages(updatedPages)
+  }
+
   return (
-    <div className="flex justify-between w-full p-5">
+    <div className="flex justify-between w-screen p-5 items-center">
       <div>
         <p className="text-white text-2xl">BLACK HOLES</p>
       </div>
@@ -17,14 +41,21 @@ export const Navbar = ({ pages }: INavbar) => {
           }
           return (
             <button
+              onClick={() => {
+                navigate(page.link)
+                setActive(i)
+              }}
               className={
-                "text-xl w-full h-full flex justify-center items-center transition duration-100 ease-in-out " + style
+                "text-lg w-full h-full flex justify-center items-center transition duration-100 ease-in-out " + style
               }
             >
-              HOME
+              {page.name}
             </button>
           )
         })}
+      </div>
+      <div>
+        <ConnectButton />
       </div>
     </div>
   )
