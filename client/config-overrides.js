126c664d60d2cdf363fd2c60ba2941c695dfde5f
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-02-17 16:08:41 +0200

    Add client

---
+++ client/config-overrides.js
@@ -0,0 +1,26 @@
+const { removeModuleScopePlugin, override, babelInclude, addWebpackPlugin } = require("customize-cra")
+const { ProvidePlugin } = require("webpack")
+const path = require("path")
+
+module.exports = function override(config, env) {
+  config = removeModuleScopePlugin()(config, env)
+
+  config = babelInclude([path.resolve("src"), path.resolve("../backend/types"), path.resolve("../backend/common")])(
+    config,
+    env,
+  )
+
+  config.resolve.fallback = {
+    url: require.resolve("url"),
+    assert: require.resolve("assert"),
+    buffer: require.resolve("buffer"),
+  }
+  config.plugins.push(
+    new ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  )
+
+  return config
+}
