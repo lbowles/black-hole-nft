7cdbeb0a8bcff41e116e4d4f6cac0313bd0a8f65
lbowles <lspbowles@gmail.com>
2023-02-17 18:34:32 +0200

    Nav mobile scaling

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -26,37 +26,44 @@ export const Navbar = () => {
     setPages(updatedPages)
   }
 
+  let nav = (
+    <div className="grid grid-cols-3 bg-black h-10 w-full ">
+      {pages.map((page, i) => {
+        let style = ""
+        if (page.active) {
+          style = "bg-gray-900 text-white hover:bg-gray-800 "
+        } else {
+          style = "bg-black text-gray-600 hover:text-white"
+        }
+        return (
+          <button
+            onClick={() => {
+              navigate(page.link)
+              setActive(i)
+            }}
+            className={
+              "text-lg w-full h-full flex justify-center items-center transition duration-100 ease-in-out " + style
+            }
+          >
+            {page.name}
+          </button>
+        )
+      })}
+    </div>
+  )
+
   return (
-    <div className="flex justify-between w-screen p-5 items-center">
-      <div>
-        <p className="text-white text-2xl">BLACK HOLES</p>
+    <div>
+      <div className="flex justify-between w-screen p-5 ">
+        <div>
+          <p className="text-white text-2xl">BLACK HOLES</p>
+        </div>
+        <div className="sm:block hidden w-80">{nav}</div>
+        <div>
+          <ConnectButton />
+        </div>
       </div>
-      <div className="grid grid-cols-3 bg-black h-10 w-80">
-        {pages.map((page, i) => {
-          let style = ""
-          if (page.active) {
-            style = "bg-gray-900 text-white hover:bg-gray-800 "
-          } else {
-            style = "bg-black text-gray-600 hover:text-white"
-          }
-          return (
-            <button
-              onClick={() => {
-                navigate(page.link)
-                setActive(i)
-              }}
-              className={
-                "text-lg w-full h-full flex justify-center items-center transition duration-100 ease-in-out " + style
-              }
-            >
-              {page.name}
-            </button>
-          )
-        })}
-      </div>
-      <div>
-        <ConnectButton />
-      </div>
+      <div className="sm:hidden block px-5">{nav}</div>
     </div>
   )
 }
