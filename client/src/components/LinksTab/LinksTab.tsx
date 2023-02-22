5ea468cf741545c8bba1c1c67834789ab2271353
lbowles <lspbowles@gmail.com>
2023-02-22 21:48:54 +0200

    opensea link

--- client/src/components/LinksTab/LinksTab.tsx
+++ client/src/components/LinksTab/LinksTab.tsx
@@ -13,7 +13,7 @@ export const LinksTab = () => {
     <div className="bg-black border-y-2 border-r-2 border-gray-800 p-[15px] w-14 grid grid-flow-row absolute top-1/2 left-0 transform -translate-y-1/2 gap-3">
       <a
         target="_blank"
-        href="https://www.opensea.com"
+        href="https://opensea.io/collection/onchain-blackholes"
         onClick={() => {
           linkClickSound()
         }}
