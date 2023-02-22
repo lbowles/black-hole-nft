c50da26f9718754ad83d85e05739fb2a5ee8dc34
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-02-22 14:50:25 +0200

    Dynamic variables on home page

--- client/src/components/home/MergingAmountGraphic/MergingAmountGraphic.tsx
+++ client/src/components/home/MergingAmountGraphic/MergingAmountGraphic.tsx
@@ -1,15 +1,27 @@
+import {
+  useBlackHolesGetBaseUpgradeMass,
+  useBlackHolesGetMintState,
+  useBlackHolesTotalMinted,
+  useBlackHolesUpgradeIntervals,
+} from "../../../generated"
+import { MintState } from "../../../interfaces/IMintState"
+
 type IMergingAmountGraphic = {
   levels: { startName: string; endName: string; startImg: string; endImg: string; amount?: number | undefined }[]
 }
 
 export const MergingAmountGraphic = ({ levels }: IMergingAmountGraphic) => {
+  const { data: totalMinted } = useBlackHolesTotalMinted()
+  const { data: baseUpgradeMass } = useBlackHolesGetBaseUpgradeMass()
+  const { data: mintState } = useBlackHolesGetMintState()
   return (
     <>
+      <p className="text-white">Total Minted: {totalMinted?.toString()}</p>
       {levels.map((level, index) => {
         return (
           <div key={index} className="w-full justify-between items-center flex pb-2">
-            {level.amount ? (
-              <p className="text-white text-5xl -mt-4">{level.amount}</p>
+            {mintState === MintState.Closed && baseUpgradeMass ? (
+              <p className="text-white text-5xl -mt-4">{index == 0 ? baseUpgradeMass.toString() : 2}</p>
             ) : (
               <p className="text-white text-5xl -mt-4">?</p>
             )}
