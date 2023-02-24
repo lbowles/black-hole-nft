bacaf1c0e5152b01fef6e0a18eb39baae63efe11
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-02-25 00:49:44 +0200

    Add Voidable Black Holes and tests

---
+++ backend/contracts/VoidableBlackHoles.sol
@@ -0,0 +1,342 @@
+// SPDX-License-Identifier: MIT
+/*                                                                                                                                                                                                                                          
+                                                                             
+                             ......................                             
+                        .....2222222222222222222222/.....                       
+                     ...222224444444444444444444444222222...                    
+                  ...22222244                      *44222222...                 
+               ...22244444*                           44444422#..               
+             ..222222444                                 44422222,..            
+             ..222444                                       44422,..            
+             ..222444                                       44422,..            
+          ..,22444                                             44222...         
+          ..,22444                                             44222...         
+          ..,22444                                             44222...         
+          ..,22444                                             44222...         
+       ...22222444                                             44222222...      
+    ...22222244444.............................................44444222222..    
+    444444444444444444444444444444444444444444444444444444444444444444444444/   
+       .................222222222222222222222222222222222.................      
+             ..222444           .................           44422,..            
+             ..222444           .................           44422,..            
+             ..222222444                                 44422222,..            
+               ...22244444*                           44444422#..               
+               ......22222244                      *44222222...                 
+                     ...222224444444444444444444444222222...                    
+                        .....2222222222222222222222/.....                       
+                             ......................                             
+*/
+
+pragma solidity ^0.8.12;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./Utilities.sol";
+import "./Renderer.sol";
+import "./BlackHoles.sol";
+import "./interfaces/BlackHole.sol";
+import "svgnft/contracts/Base64.sol";
+import "./interfaces/IERC4906Lean.sol";
+
+contract VoidableBlackHoles is ERC721, Ownable, IERC4906Lean {
+  error NotAllowed();
+  error URIQueryForNonexistentToken();
+
+  uint256 public mergeOpenTimestamp;
+
+  mapping(uint256 => uint256) public massesConsumed;
+
+  Renderer public renderer;
+  BlackHoles public unmigratedBlackHoles;
+
+  uint256 minted;
+  uint256 burned;
+
+  /**
+   * @dev Constructs a new instance of the contract.
+   * @param _name Name of the ERC721 token.
+   * @param _symbol Symbol of the ERC721 token.
+   * @param _mergeOpenTimestamp Timestamp when merging is open.
+   * @param _renderer Address of the Renderer contract.
+   * @param _unmigratedBlackHoles Address of Black Holes contract.
+   */
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint256 _mergeOpenTimestamp,
+    address _renderer,
+    address _unmigratedBlackHoles
+  ) ERC721(_name, _symbol) {
+    mergeOpenTimestamp = _mergeOpenTimestamp;
+    renderer = Renderer(_renderer);
+    unmigratedBlackHoles = BlackHoles(_unmigratedBlackHoles);
+  }
+
+  /**
+   * @notice Sets the merging delay.
+   * @param _mergeOpenTimestamp Timestamp when merging is open.
+   */
+  function setMergeOpenTimestamp(uint256 _mergeOpenTimestamp) external onlyOwner {
+    mergeOpenTimestamp = _mergeOpenTimestamp;
+  }
+
+  /**
+   * @notice Returns the token URI for a given token ID.
+   * @param _tokenId ID of the token to get the URI for.
+   * @return Token URI.
+   */
+  function tokenURI(uint256 _tokenId) public view virtual override(ERC721) returns (string memory) {
+    if (!_exists(_tokenId)) revert URIQueryForNonexistentToken();
+
+    string memory name = string(abi.encodePacked("BlackHole #", utils.uint2str(_tokenId)));
+    string memory description = "Fully on-chain, procedurally generated, animated black holes.";
+
+    BlackHole memory blackHole = blackHoleForTokenId(_tokenId);
+
+    string memory svg = renderer.getBlackHoleSVG(blackHole);
+
+    string memory attributes = string.concat(
+      "[",
+      '{"trait_type": "Level", "value": ',
+      utils.uint2str(blackHole.level),
+      "},",
+      '{"trait_type": "Name", "value": "',
+      blackHole.name,
+      '"},',
+      '{"trait_type": "Mass", "value": ',
+      utils.uint2str(blackHole.mass),
+      "}]"
+    );
+
+    string memory json = string(
+      abi.encodePacked(
+        '{"name":"',
+        name,
+        '","description":"',
+        description,
+        '",',
+        '"attributes": ',
+        attributes, // attributes
+        ', "image": "data:image/svg+xml;base64,',
+        Base64.encode(bytes(svg)),
+        '"}'
+      )
+    );
+    return string(abi.encodePacked("data:application/json;base64,", Base64.encode(bytes(json))));
+  }
+
+  /**
+   * @notice Get the structured representation of a token by its ID.
+   * @param _tokenId ID of the token.
+   * @return BlackHole Structured representation of the token.
+   */
+  function blackHoleForTokenId(uint256 _tokenId) public view returns (BlackHole memory) {
+    uint256 mass = massForTokenId(_tokenId);
+    uint256 level = levelForMass(mass);
+    string memory name = nameForBlackHoleLevel(level);
+    uint256 adjustment = getAdjustmentForMass(mass);
+
+    return
+      BlackHole({
+        tokenId: _tokenId,
+        level: level,
+        size: renderer.PIXELS_PER_SIDE() / 2 - (10 - level),
+        mass: mass,
+        name: name,
+        adjustment: adjustment
+      });
+  }
+
+  /**
+   * @notice Gets adjustment for a given mass.
+   * @param _mass Mass to calculate the adjustment for.
+   * @return Adjustment.
+   */
+  function getAdjustmentForMass(uint256 _mass) public view returns (uint256) {
+    uint256 level = levelForMass(_mass); // 1
+
+    if (level == unmigratedBlackHoles.MAX_LEVEL()) {
+      return 0;
+    }
+
+    int256 maxAdjustment = 20;
+    int256 adjustment = 0;
+    uint256[] memory upgradeIntervals = getUpgradeIntervals();
+
+    if (level == 0) {
+      adjustment = (-maxAdjustment * int256(_mass - 1)) / int256(upgradeIntervals[0] - 2) + maxAdjustment;
+    } else {
+      adjustment =
+        (-maxAdjustment * int256(_mass - upgradeIntervals[level - 1])) /
+        int256(upgradeIntervals[level] - upgradeIntervals[level - 1] - 1) +
+        maxAdjustment;
+    }
+
+    return uint256(adjustment);
+  }
+
+  /**
+   * @notice Gets the level for a given mass.
+   */
+  function levelForMass(uint256 _mass) public view returns (uint256) {
+    uint256[] memory upgradeIntervals = getUpgradeIntervals();
+
+    if (_mass < upgradeIntervals[0]) {
+      return 0;
+    } else if (_mass < upgradeIntervals[1]) {
+      return 1;
+    } else if (_mass < upgradeIntervals[2]) {
+      return 2;
+    } else if (_mass < upgradeIntervals[3]) {
+      return 3;
+    } else {
+      return 4;
+    }
+  }
+
+  function massForTokenId(uint256 _tokenId) public view returns (uint256) {
+    return massesConsumed[_tokenId] + 1;
+  }
+
+  /**
+   * @notice Returns the mass required to upgrade to the next level for each level.
+   */
+  function getUpgradeIntervals() public view returns (uint256[] memory) {
+    uint256 baseUpgradeMass = utils.max(
+      unmigratedBlackHoles.totalMinted() /
+        unmigratedBlackHoles.MAX_SUPPLY_OF_INTERSTELLAR() /
+        2**(unmigratedBlackHoles.MAX_LEVEL() - 1),
+      2
+    );
+    uint256[] memory intervals = new uint256[](unmigratedBlackHoles.MAX_LEVEL() + 1);
+    intervals[0] = utils.max(baseUpgradeMass / 2, 2);
+    intervals[1] = baseUpgradeMass * 2;
+    intervals[2] = baseUpgradeMass * 4;
+    intervals[3] = baseUpgradeMass * 8;
+    return intervals;
+  }
+
+  /**
+   * @notice Returns the name of a black hole level.
+   * @param _level Level of the black hole.
+   * @return name of the black hole level.
+   */
+  function nameForBlackHoleLevel(uint256 _level) public view returns (string memory) {
+    return unmigratedBlackHoles.nameForBlackHoleLevel(_level);
+  }
+
+  /**
+   * @notice Migrate Black Holes to Voidable Black Holes by burning the Black Holes.
+   *         Requires the Approval of this contract on the Edition contract.
+   * @param tokenIds The Edition token IDs you want to migrate.
+   */
+  function mint(uint256[] calldata tokenIds) external {
+    uint256 count = tokenIds.length;
+
+    // Burn the Black Holes for the given tokenIds & mint the Voidable Black Holes.
+    for (uint256 i; i < count; ) {
+      uint256 id = tokenIds[i];
+      address owner = unmigratedBlackHoles.ownerOf(id);
+
+      // Check whether we're allowed to migrate this Black Hole.
+      if (
+        owner != msg.sender &&
+        (!unmigratedBlackHoles.isApprovedForAll(owner, msg.sender)) &&
+        unmigratedBlackHoles.getApproved(id) != msg.sender
+      ) {
+        revert NotAllowed();
+      }
+
+      // Burn old.
+      unmigratedBlackHoles.transferFrom(owner, address(1), id);
+
+      // Mint new.
+      _safeMint(msg.sender, id);
+
+      unchecked {
+        ++i;
+      }
+    }
+
+    // Keep track of how many checks have been minted.
+    unchecked {
+      minted += uint32(count);
+    }
+  }
+
+  /**
+   * @notice Returns whether merging is enabled or not.
+   */
+  function isMergingEnabled() public view returns (bool) {
+    return block.timestamp > mergeOpenTimestamp;
+  }
+
+  /**
+   * @notice Merges a list of tokens into a single token.
+   * @param tokens List of token IDs to merge. The first token in the list is the target.
+   */
+  function merge(uint256[] memory tokens) public {
+    // Burn all tokens except the first one, aka the target
+    // The mass of all other tokens get added to the target
+    require(isMergingEnabled(), "Merging not enabled");
+    require(tokens.length > 1, "Must merge at least 2 tokens");
+
+    uint256 targetId = tokens[0];
+
+    require(ownerOf(tokens[0]) == msg.sender, "Must own all tokens (target)");
+
+    uint256 sum;
+    for (uint256 i = 1; i < tokens.length; ) {
+      require(ownerOf(tokens[i]) == msg.sender, "Must own all tokens (burn)");
+      sum = sum + massForTokenId(tokens[i]);
+      massesConsumed[tokens[i]] = 0;
+      _burn(tokens[i]);
+
+      unchecked {
+        ++i;
+      }
+    }
+
+    massesConsumed[targetId] += sum;
+
+    emit MetadataUpdate(targetId);
+  }
+
+  /**
+   * @notice Returns Black Holes for a list of token IDs.
+   * @param tokenIds List of token IDs.
+   */
+  function blackHolesForTokenIds(uint256[] memory tokenIds) public view returns (BlackHole[] memory) {
+    BlackHole[] memory blackHoles = new BlackHole[](tokenIds.length);
+    for (uint256 i = 0; i < tokenIds.length; i++) {
+      blackHoles[i] = blackHoleForTokenId(tokenIds[i]);
+    }
+    return blackHoles;
+  }
+
+  function burn(uint256 tokenId) public {
+    require(ownerOf(tokenId) == msg.sender, "Must own token");
+    _burn(tokenId);
+  }
+
+  /**
+   * @notice Returns total number of tokens minted.
+   */
+  function totalMinted() external view returns (uint256) {
+    return minted;
+  }
+
+  /**
+   * @notice Returns total number of tokens burned.
+   */
+  function totalBurned() external view returns (uint256) {
+    return burned;
+  }
+
+  /**
+   * @notice Returns how many tokens this contract manages.
+   */
+  function totalSupply() public view returns (uint256) {
+    return minted - burned;
+  }
+}
---
+++ backend/test/VoidableBlackHoles.test.ts
@@ -0,0 +1,270 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { expect } from "chai"
+import { BigNumber } from "ethers"
+import { XMLParser } from "fast-xml-parser"
+import { deployments, ethers } from "hardhat"
+import { BlackHoles, BlackHoles__factory, VoidableBlackHoles, VoidableBlackHoles__factory } from "../types"
+
+describe.only("VoidableBlackHoles", function () {
+  let signers: SignerWithAddress[]
+  let voidableBlackHoles: VoidableBlackHoles
+  let blackHoles: BlackHoles
+  let beforeMerge: BigNumber
+
+  beforeEach(async function () {
+    await ethers.provider.send("evm_setAutomine", [true])
+    await deployments.fixture(["VoidableBlackHoles"])
+    signers = await ethers.getSigners()
+    const VoidableBlackHoles = await deployments.get("VoidableBlackHoles")
+    voidableBlackHoles = VoidableBlackHoles__factory.connect(VoidableBlackHoles.address, signers[0])
+
+    const BlackHoles = await deployments.get("BlackHoles")
+    blackHoles = BlackHoles__factory.connect(BlackHoles.address, signers[0])
+
+    /* Complete sale (mint 6,700 tokens) */
+    expect(await blackHoles.isMergingEnabled()).to.be.false
+
+    // Mint in threshold sale
+    let mintPrice = await blackHoles.getPrice()
+    let threshold = await blackHoles.timedSaleThreshold()
+    await blackHoles.mint(threshold, { value: mintPrice.mul(threshold) })
+
+    // Mint up to 6,700 tokens in timed sale
+    mintPrice = await blackHoles.getPrice()
+    let totalMinted = await blackHoles.totalMinted()
+    let remaining = 6700 - totalMinted.toNumber()
+    await blackHoles.mint(remaining, { value: mintPrice.mul(remaining) })
+
+    expect(await blackHoles.totalMinted()).to.equal(6700)
+
+    // Progress time by timed sale duration and merge delay
+    const timedSaleStartTimestamp = await blackHoles.timedSaleStartedTimestamp()
+    const mergingDelay = await blackHoles.mergingDelay()
+    const mergeOpenTimestamp = timedSaleStartTimestamp.add(mergingDelay)
+
+    await voidableBlackHoles.setMergeOpenTimestamp(mergeOpenTimestamp)
+
+    // Take snapshot
+    beforeMerge = await ethers.provider.send("evm_snapshot", [])
+    await ethers.provider.send("evm_increaseTime", [mergeOpenTimestamp.toNumber()])
+    await ethers.provider.send("evm_mine", [])
+  })
+
+  /**
+   * Colours
+   */
+
+  it("Should wait for the merge delay to pass before merging", async function () {
+    // Revert to beforeMerge snapshot
+    await ethers.provider.send("evm_revert", [beforeMerge])
+    // Get merge timestamp
+    const mergeTimestamp = await voidableBlackHoles.mergeOpenTimestamp()
+    const currentTimestamp = await ethers.provider.getBlock("latest").then((block) => block.timestamp)
+
+    expect(await voidableBlackHoles.isMergingEnabled()).to.be.false
+
+    await ethers.provider.send("evm_increaseTime", [mergeTimestamp.sub(currentTimestamp).toNumber() + 1])
+    await ethers.provider.send("evm_mine", [])
+
+    expect(await voidableBlackHoles.isMergingEnabled()).to.be.true
+  })
+
+  it("Should migrate tokens from the old contract to the new contract", async function () {
+    await blackHoles.setApprovalForAll(voidableBlackHoles.address, true)
+
+    expect(await voidableBlackHoles.mint([1, 2, 3, 4]))
+      // Burn
+      .to.emit(blackHoles, "Transfer")
+      .withArgs(signers[0].address, ethers.constants.One, 1)
+      .and.to.emit(blackHoles, "Transfer")
+      .withArgs(signers[0].address, ethers.constants.One, 2)
+      .and.to.emit(blackHoles, "Transfer")
+      .withArgs(signers[0].address, ethers.constants.One, 3)
+      .and.to.emit(blackHoles, "Transfer")
+      .withArgs(signers[0].address, ethers.constants.One, 4)
+      // Mint
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(ethers.constants.AddressZero, signers[0].address, 1)
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(ethers.constants.AddressZero, signers[0].address, 2)
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(ethers.constants.AddressZero, signers[0].address, 3)
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(ethers.constants.AddressZero, signers[0].address, 4)
+
+    expect(await voidableBlackHoles.balanceOf(signers[0].address)).to.equal(4)
+    expect(await voidableBlackHoles.totalSupply()).to.equal(4)
+    expect(await voidableBlackHoles.totalMinted()).to.equal(4)
+  })
+
+  it("Should return a valid token URI for a given token ID", async function () {
+    // Mint a new token
+    await blackHoles.setApprovalForAll(voidableBlackHoles.address, true)
+    await voidableBlackHoles.mint([1])
+
+    const tokenId = 1
+    const name = "BlackHole #" + tokenId
+    const description = "Fully on-chain, procedurally generated, animated black holes."
+    const metadata = await voidableBlackHoles.tokenURI(tokenId)
+
+    // Decode base64 encoded json
+    const decoded = Buffer.from(metadata.split(",")[1], "base64").toString()
+    const json = JSON.parse(decoded)
+
+    expect(json.name).to.equal(name)
+    expect(json.description).to.equal(description)
+    expect(json.image).to.contain("data:image/svg+xml;base64")
+
+    // console.log(json.image)
+    const svg = Buffer.from(json.image.split(",")[1], "base64").toString()
+    const parser = new XMLParser()
+    expect(parser.parse(svg, true)).to.not.throw
+  })
+
+  it("Should merge tokens", async function () {
+    expect(await voidableBlackHoles.unmigratedBlackHoles()).to.equal(blackHoles.address)
+
+    expect(await voidableBlackHoles.isMergingEnabled()).to.be.true
+
+    // Migrate tokens
+    await blackHoles.setApprovalForAll(voidableBlackHoles.address, true)
+
+    // Numbers from 1 to 10
+    let tokenIds = Array.from(Array(400).keys()).map((n) => n + 1)
+    expect(await voidableBlackHoles.mint(tokenIds))
+
+    /* Merge */
+    // Update correct token's metadata and burn the right tokens
+    expect(await voidableBlackHoles.merge([1, 2, 3, 4]))
+      .to.emit(voidableBlackHoles, "MetadataUpdate")
+      .withArgs(1)
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(2, ethers.constants.AddressZero, signers[0].address)
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(3, ethers.constants.AddressZero, signers[0].address)
+      .and.to.emit(voidableBlackHoles, "Transfer")
+      .withArgs(4, ethers.constants.AddressZero, signers[0].address)
+
+    let tokenMetadata = await voidableBlackHoles.blackHoleForTokenId(1)
+    expect(tokenMetadata.mass).to.equal(4)
+
+    const upgradeIntervals = await voidableBlackHoles.getUpgradeIntervals()
+
+    console.log(upgradeIntervals)
+
+    let lastTokenId = 4
+
+    let totalGas = BigNumber.from(0)
+
+    for (let level = 0; level < 4; level++) {
+      // Merge to next level
+      // Array of numbers from 5 to 12
+      const mergeIds = Array.from(Array(upgradeIntervals[level].toNumber()).keys()).map((i) => i + lastTokenId + 1)
+      const tx = await voidableBlackHoles.merge([1, ...mergeIds])
+      const receipt = await tx.wait()
+
+      totalGas = totalGas.add(receipt.gasUsed)
+
+      tokenMetadata = await voidableBlackHoles.blackHoleForTokenId(1)
+      console.log(tokenMetadata)
+      expect(tokenMetadata.level).to.equal(level + 1)
+
+      lastTokenId = mergeIds[mergeIds.length - 1]
+    }
+
+    console.log("Total gas used:", totalGas.toString())
+  })
+
+  it("Should return the correct colour adjustment for a given mass", async function () {
+    const upgradeIntervals = await voidableBlackHoles.getUpgradeIntervals()
+    console.log(upgradeIntervals.map((i) => i.toNumber()))
+    for (let i = 1; i <= upgradeIntervals[3].toNumber(); i += 1) {
+      const adjustment = await voidableBlackHoles.getAdjustmentForMass(i)
+      // Expect the adjustment to be between 0 and and 20
+      expect(adjustment).to.be.gte(0).and.lte(20)
+      // Expect the adjustment to be 0 when the mass is equal to one of the upgrade intervals - 1
+      if (upgradeIntervals.find((interval, index) => interval.toNumber() === i && index !== 0 && index !== 3)) {
+        expect(adjustment).to.equal(20)
+      }
+      if (upgradeIntervals.find((interval, index) => interval.toNumber() - 1 === i && index !== 0 && index !== 3)) {
+        console.log("adjustment for mass", i, "is", adjustment.toString())
+        expect(adjustment).to.equal(0)
+      }
+
+      // For mass of 1 it should be 20
+      if (i === 1) {
+        expect(adjustment).to.equal(20)
+      }
+
+      // For mass of upgradeIntervals[3].toNumber() it should be 0
+      if (i === upgradeIntervals[3].toNumber()) {
+        expect(adjustment).to.equal(0)
+      }
+    }
+  })
+
+  // it("Should provide the correct names for each level", async function () {
+  //   const names = ["Micro", "Stellar", "Intermediate", "Supermassive", "Primordial"]
+
+  //   for (let i = 0; i < 5; i++) {
+  //     expect(await voidableBlackHoles.nameForBlackHoleLevel(i)).to.equal(names[i])
+  //   }
+  // })
+
+  // it("Should provide the correct level for each mass", async function () {
+  //   // Mint 1,000 tokens
+  //   mintPrice = await voidableBlackHoles.getPrice()
+  //   await voidableBlackHoles.mint(10_000, { value: mintPrice.mul(10_000) })
+
+  //   // Get base upgrade mass
+  //   const baseUpgradeMass = (await voidableBlackHoles.getBaseUpgradeMass()).toNumber()
+
+  //   // Check each level
+  //   for (let level = 0; level < 4; level++) {
+  //     const mass = baseUpgradeMass * 2 ** level
+  //     expect(await voidableBlackHoles.levelForMass(mass)).to.equal((level + 1).toString())
+  //   }
+  // })
+
+  // it("Should show different colors for different levels with the same mass", async function () {
+  //   // Mint threshold tokens
+  //   mintPrice = await voidableBlackHoles.getPrice()
+  //   await voidableBlackHoles.mint(threshold, { value: threshold.mul(mintPrice) })
+
+  //   // Advance time to merging
+  //   const timedSaleDuration = await voidableBlackHoles.timedSaleDuration()
+  //   await ethers.provider.send("evm_increaseTime", [timedSaleDuration.toNumber()])
+
+  //   const mergingDelay = await voidableBlackHoles.mergingDelay()
+  //   await ethers.provider.send("evm_increaseTime", [mergingDelay.toNumber()])
+  //   await ethers.provider.send("evm_mine", [])
+
+  //   await voidableBlackHoles.merge([1, 2, 3, 4])
+
+  //   // Get token uri 1 and token uri 5
+  //   const tokenUri1 = await voidableBlackHoles.tokenURI(1)
+  //   const tokenUri5 = await voidableBlackHoles.tokenURI(5)
+  //   const tokenUri6 = await voidableBlackHoles.tokenURI(6)
+
+  //   function getStyle(metadata: string) {
+  //     // Decode base64 encoded json
+  //     const decoded = Buffer.from(metadata.split(",")[1], "base64").toString()
+  //     const json = JSON.parse(decoded)
+  //     // console.log(json.image)
+  //     const svg = Buffer.from(json.image.split(",")[1], "base64").toString()
+  //     const style = svg.split("<style>")[1].split("</style>")[0]
+  //     return style
+  //   }
+
+  //   // Get SVGs
+  //   const style1 = getStyle(tokenUri1)
+  //   const style5 = getStyle(tokenUri5)
+  //   const style6 = getStyle(tokenUri6)
+
+  //   // Merged token different to unmerged token
+  //   expect(style1).to.not.equal(style5)
+
+  //   // Two unmerged tokens have the same style
+  //   expect(style6).to.equal(style5)
+  // })
+})
