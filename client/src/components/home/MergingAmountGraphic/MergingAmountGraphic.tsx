091eacc8d782b2f9bddb706a1cdadacd9ce3b220
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-02-25 14:12:32 +0200

    Upgrade intervals om home page

--- client/src/components/home/MergingAmountGraphic/MergingAmountGraphic.tsx
+++ client/src/components/home/MergingAmountGraphic/MergingAmountGraphic.tsx
@@ -1,10 +1,4 @@
-import {
-  useBlackHolesGetBaseUpgradeMass,
-  useBlackHolesGetMintState,
-  useBlackHolesTotalMinted,
-  useBlackHolesUpgradeIntervals,
-} from "../../../generated"
-import { MintState } from "../../../interfaces/IMintState"
+import { useBlackHolesTotalMinted, useVoidableBlackHolesGetUpgradeIntervals } from "../../../generated"
 
 type IMergingAmountGraphic = {
   levels: { startName: string; endName: string; startImg: string; endImg: string; amount?: number | undefined }[]
@@ -12,19 +6,27 @@ type IMergingAmountGraphic = {
 
 export const MergingAmountGraphic = ({ levels }: IMergingAmountGraphic) => {
   const { data: totalMinted } = useBlackHolesTotalMinted()
-  const { data: baseUpgradeMass } = useBlackHolesGetBaseUpgradeMass()
+  const { data: upgradeIntervals } = useVoidableBlackHolesGetUpgradeIntervals()
   return (
     <>
       <p className="text-white text-2xl">AS IT STANDS: {totalMinted?.toNumber().toLocaleString()} MINTED</p>
       <p className="text-gray-600 text-xl">{new Date().toLocaleString()}</p>
       {/* {<p className="text-white text-xl mt-4">{totalMinted?.toNumber().toLocaleString()} MINTED</p>} */}
-      {levels.map((level, index) => {
+      {levels.map((level, levelNumber) => {
+        console.log(level)
+
+        let microsForLevel = ""
+        if (upgradeIntervals) {
+          if (levelNumber === 0) {
+            microsForLevel = upgradeIntervals[levelNumber].toString()
+          } else {
+            microsForLevel = (upgradeIntervals[levelNumber].toNumber() / upgradeIntervals[0].toNumber()).toFixed(0)
+          }
+        }
         return (
-          <div key={index} className="w-full justify-between items-center flex pb-2">
-            {baseUpgradeMass ? (
-              <p className="text-white text-5xl -mt-4 text-right w-1/4">
-                {index == 0 ? baseUpgradeMass.toString() : 2}
-              </p>
+          <div key={levelNumber} className="w-full justify-between items-center flex pb-2">
+            {upgradeIntervals ? (
+              <p className="text-white text-5xl -mt-4 text-right w-1/4">{upgradeIntervals[levelNumber].toNumber()}</p>
             ) : (
               <p className="text-white text-5xl -mt-4 text-right w-1/4">?</p>
             )}
